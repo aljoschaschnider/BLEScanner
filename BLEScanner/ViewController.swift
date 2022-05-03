7bd71804a6c5410f0d213197fbd30f2358d3fcdc
Aljoscha Schnider <rachet@gmx.ch>
2022-05-03 10:22:50 +0200

    Initial Commit

---
+++ BLEScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BLEScanner
+//
+//  Created by Aljoscha Schnider on 03.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
