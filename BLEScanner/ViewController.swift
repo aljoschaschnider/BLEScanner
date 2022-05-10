60887671c5edc1efddf942983ac1daf1d67e9cf4
aljoschaschnider <rachet@gmx.ch>
2022-05-10 14:05:56 +0200

    Scan now possible

--- BLEScanner/ViewController.swift
+++ BLEScanner/ViewController.swift
@@ -6,13 +6,34 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CBCentralManagerDelegate {
+    private var centralManager: CBCentralManager!
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            print("Bluetooth is On")
+            centralManager.scanForPeripherals(withServices: nil, options: nil)
+        } else {
+            print("Bluetooth is not active")
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        centralManager = CBCentralManager(delegate: self, queue: nil, options: nil)
         // dont do anything
         // Do any additional setup after loading the view.
+    }
+    
+    public func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        print("\nName   : \(peripheral.name ?? "(No name)")")
+        print("RSSI   : \(RSSI)")
+        for ad in advertisementData {
+            print("AD Data: \(ad)")
+        }
     }
 
 
